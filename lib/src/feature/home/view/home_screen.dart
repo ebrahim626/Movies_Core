7004dc94c4b15c72a55931536524e47440230ab5
aktaruzzaman <ebrahimhossain626@gmail.com>
2026-09-25 23:01:32 +0600

    home screen upper bar complete

--- lib/src/feature/home/view/home_screen.dart
+++ lib/src/feature/home/view/home_screen.dart
@@ -5,6 +5,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 16, vertical: 22),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(backgroundColor: Colors.red),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Aktaruzzaman"),
+                      Text("Let's watch a movie"),
+                    ],
+                  ),
+                  Spacer(),
+                  Icon(Icons.search),
+                  SizedBox(width: 6),
+                  Icon(Icons.notifications_none_outlined),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
